49c9c058d0ced8b5dee3189491c1e3a7d1b2207b
chintu1997 <bathini.rohith97@gmail.com>
2024-10-06 18:55:53 +0100

    Updated VPC module to use dynamic availability zones

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 data "aws_availability_zones" "available" {}
 
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
@@ -8,7 +7,7 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b"]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
